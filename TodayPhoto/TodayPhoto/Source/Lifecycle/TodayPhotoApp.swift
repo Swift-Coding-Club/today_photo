d07a626eb69e5c1184035983c671e8681ad841e4
Drew <dongyoon.dev@gmail.com>
2023-01-07 15:32:52 +0900

    :wrench: Basic file setting

---
+++ TodayPhoto/TodayPhoto/Source/Lifecycle/TodayPhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodayPhotoApp.swift
+//  TodayPhoto
+//
+//  Created by 김동윤 on 2023/01/07.
+//
+
+import SwiftUI
+
+@main
+struct TodayPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TodayPhoto/TodayPhoto/Source/Views/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TodayPhoto
+//
+//  Created by 김동윤 on 2023/01/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
