d2f24099daad7c0d2c9f039d0d4639c9c94cd6c9
Drew <dongyoon.dev@gmail.com>
2023-01-19 19:19:41 +0900

    :wrench: :: Add ColorExtension

---
+++ TodayPhoto/TodayPhoto/Source/Utils/Extensions/ColorExtension.swift
@@ -0,0 +1,33 @@
+//
+//  ColorExtension.swift
+//  TodayPhoto
+//
+//  Created by 김동윤 on 2023/01/19.
+//
+
+import SwiftUI
+
+extension Color {
+    // PrimaryBlack
+    static let primaryBlackDark: Color = Color("PrimaryBlackDark")
+    static let primaryBlack100: Color = Color("PrimaryBlack100")
+    static let primaryBlack80: Color = Color("PrimaryBlack80")
+    static let primaryBlack60: Color = Color("PrimaryBlack60")
+    static let primaryBlack40: Color = Color("PrimaryBlack40")
+    static let primaryBlack20: Color = Color("PrimaryBlack20")
+    static let primaryBlack10: Color = Color("PrimaryBlack10")
+    
+    // PrimaryPurple
+    static let primarPurpleDark: Color = Color("PrimaryPurpleDark")
+    static let primarPurple100: Color = Color("PrimaryPurple100")
+    static let primarPurple80: Color = Color("PrimaryPurple80")
+    static let primarPurple60: Color = Color("PrimaryPurple60")
+    static let primarPurple40: Color = Color("PrimaryPurple40")
+    static let primarPurple20: Color = Color("PrimaryPurple20")
+    static let primarPurple10: Color = Color("PrimaryPurple10")
+    
+    // System Color
+    static let ststemBlue: Color = Color("SystemBlue")
+    static let ststemRed: Color = Color("SystemRed")
+}
+
