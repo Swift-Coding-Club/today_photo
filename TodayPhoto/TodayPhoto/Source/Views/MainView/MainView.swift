ee5640aa37bc709d412cea1c75451cc64edf1202
Drew <dongyoon.dev@gmail.com>
2023-02-16 20:49:24 +0900

    :wrench: :: Add MainView file

---
+++ TodayPhoto/TodayPhoto/Source/Views/MainView/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  TodayPhoto
+//
+//  Created by 김동윤 on 2023/02/15.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
