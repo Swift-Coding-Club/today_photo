b98ccf95dff009bac2a734112aa1cc04ba4811db
Drew <dongyoon.dev@gmail.com>
2023-02-17 23:03:55 +0900

    :wrench: :: Add FeedView file

---
+++ TodayPhoto/TodayPhoto/Source/Views/MainView/FeedView.swift
@@ -0,0 +1,20 @@
+//
+//  FeedView.swift
+//  TodayPhoto
+//
+//  Created by 김동윤 on 2023/02/16.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+struct FeedView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedView()
+    }
+}
