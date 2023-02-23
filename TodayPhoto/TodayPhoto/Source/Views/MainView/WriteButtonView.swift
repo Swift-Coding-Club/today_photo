ce2bed9604c6545faf204a4cbab6f3f29d4f6b62
Drew <dongyoon.dev@gmail.com>
2023-02-23 14:47:47 +0900

    :wrench: :: Add WriteButtonView file

---
+++ TodayPhoto/TodayPhoto/Source/Views/MainView/WriteButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  WriteButtonView.swift
+//  TodayPhoto
+//
+//  Created by 김동윤 on 2023/02/23.
+//
+
+import SwiftUI
+
+struct WriteButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WriteButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        WriteButtonView()
+    }
+}
